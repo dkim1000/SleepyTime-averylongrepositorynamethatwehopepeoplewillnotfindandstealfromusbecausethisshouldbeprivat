fda8a018a8306da1cb7018c56f0e1acea69d27b9
David Kim <TheGreatKingDK@Davids-MacBook-Pro.local>
2014-08-02 20:49:20 -0400

    Changed default template. Also temporarily set default layout to be the alarm

--- SleepRight/app/src/androidTest/java/alarm/com/sleepright/ApplicationTest.java
+++ SleepRight/app/src/androidTest/java/alarm/com/sleepright/ApplicationTest.java
@@ -1,4 +1,4 @@
-package SleepRight.com.sleepright;
+package alarm.com.sleepright;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- SleepRight/app/src/main/java/SleepRight/com/sleepright/Alarm.java
+++
@@ -1,7 +0,0 @@
-package SleepRight.com.sleepright;
-
-/**
- * Created by TheGreatKingDK on 2014-08-01.
- */
-public class Alarm {
-}
